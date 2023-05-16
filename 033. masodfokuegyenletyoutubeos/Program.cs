caf9338d95ee1a37ad12b7c58e141ac3157b5c48
DGYKKSZKI <124397426+Rattlesnake19720612@users.noreply.github.com>
2023-05-16 22:19:52 +0200

    Másodfokú egyenlet (CLI)

--- 033. masodfokuegyenletyoutubeos/Program.cs
+++ 033. masodfokuegyenletyoutubeos/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -45,3 +45,20 @@ namespace _033.masodfokuegyenletyoutubeos
         }
     }
 }
+
+/*Ez egy C# program, amely megoldja a másodfokú egyenletet és kiszámítja annak gyökeit. A program bekéri az "a", "b" és "c" értékeket a felhasználótól, majd elvégzi a szükséges számításokat és kiírja a gyökértékeket.
+
+Íme, hogyan működik a program lépésről lépésre:
+
+A program importálja a szükséges könyvtárakat.
+A program létrehozza a Main függvényt, amely a program belépési pontja.
+Deklarálja és inicializálja az a, b, c, d, x1 és x2 változókat a megfelelő típussal.
+A program kéri a felhasználótól az a értékét, majd átalakítja azt double típusba a Convert.ToDouble metódus segítségével.
+Az if-else struktúra segítségével ellenőrzi, hogy az a értéke nulla-e. Ha igen, kiírja, hogy az egyenlet nem lehet másodfokú. Ha nem, akkor folytatja a számításokkal.
+A program kéri a felhasználótól a b és c értékeit, majd átalakítja azokat double típusba.
+Kiszámítja a d értékét a másodfokú egyenlet diszkriminánsaként: d = b * b - 4 * a * c.
+Az if-else struktúra segítségével ellenőrzi, hogy a diszkrimináns kisebb-e mint 0. Ha igen, kiírja, hogy nincs valós megoldás. Ha nem, akkor folytatja a gyökértékek kiszámításával.
+Kiszámítja az x1 és x2 értékeit a másodfokú egyenlet gyökformulájával: x1 = (-b + Math.Sqrt(d)) / (2 * a) és x2 = (-b - Math.Sqrt(d)) / (2 * a).
+Kiírja az eredményt a konzolra a Console.WriteLine metódussal.
+A program várja, hogy a felhasználó nyomjon meg egy billentyűt a kilépéshez.
+Ezután a program kilép.*/
